c51ca5ee15b21949712c1326eb7d8e677917f374
Fasil Minale <fasiladam1290@gmail.com>
2022-09-07 14:34:25 +0300

    update the schema

--- src/schema.js
+++ src/schema.js
@@ -43,9 +43,9 @@ const typeDefs = gql`
     description: String
     "The number of times a track has been viewed"
     numberOfViews: Int
-    "The track's complete array of Modules"
     "The track's full duration, in seconds"
-durationInSeconds: Int @deprecated(reason: "Use durationInSeconds")
+    durationInSeconds: Int @deprecated(reason: "Use durationInSeconds")
+    "The track's complete array of Modules"
     modules: [Module!]!
   }
 
@@ -64,7 +64,7 @@ durationInSeconds: Int @deprecated(reason: "Use durationInSeconds")
     "The module's title"
     title: String!
     "The module's length in minutes"
-    length: Int @deprecated(reason: "Use durationInSeconds")
+    length: Int
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video duration in seconds"
